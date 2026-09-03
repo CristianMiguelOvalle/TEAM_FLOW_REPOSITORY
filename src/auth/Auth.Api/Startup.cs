9476f7684493ac3b175dfbb1d4aed03d677d8ace
Cristian <323880937+CristianMiguelOvalle@users.noreply.github.com>
2026-09-03 15:45:46 -0400

    Add swagger and some libraries + Create startup class

---
+++ src/auth/Auth.Api/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace Auth.Api
+{
+    public class Startup
+    {
+        public static void ConfigureServices(
+        IServiceCollection services,
+        IConfiguration configuration)
+        {
+            services.AddControllers();
+            services.AddOpenApi();
+            services.AddSwaggerGen();
+        }
+
+        public static void Configure(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
